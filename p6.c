240550ffc0491ffa49fd62fa074c5a5403501be4
sem-7 <78492134+sem-7@users.noreply.github.com>
2021-02-04 20:29:47 +0530

    Update p6.c

--- p6.c
+++ p6.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <omp.h>
 #include <string.h>
+#include <ctype.h>
 #define COUNT 10
 
 char search_words[20][COUNT] = {"The","around","graphics","from","by","be","any",
@@ -8,85 +9,66 @@ char search_words[20][COUNT] = {"The","around","graphics","from","by","be","any"
 long counts[COUNT];
 int line_c = 0;
 
-int is_alpha(char c) {
-	return ((c >=  65 && c <=  90) || (c >=  97 && c <=  122));
-}
-
-int is_equal(char* a,const char* key, int ignore_case) {
+int is_equal(char *a, const char *key, int ignore_case) {
 	char b[20];
-	strcpy(b,key);
-	int len_a = strlen(a),len_b = strlen(b);
+	strcpy(b, key);
+	int len_a = strlen(a), len_b = strlen(b);
 
-	if(len_a !=  len_b) {
+	if(len_a != len_b)
 		return 0;
-	}
-	if(ignore_case != 0) {
-		int i;
-		for(i = 0; i < len_a; i++) {
-			if(a[i] > 90)
-				a[i] -=  32;
-		}
-		for(i = 0; i < len_b; i++) {
-			if(b[i] > 90)
-				b[i] -=  32;
+	else if (ignore_case) {
+		for(int i = 0; i < len_a; i++) {
+			a[i] = tolower(a[i]);
+			b[i] = tolower(b[i]);
 		}
 	}
-	return (strcmp(a,b) ==  0);
+	return (strcmp(a, b) == 0);
 }
+
 void read_word(char *temp, FILE *fp) {
 	int i = 0;
 	char ch;
 
-	while((ch = fgetc(fp)) !=  EOF && is_alpha(ch) ==  0);
+	while((ch = fgetc(fp)) != EOF && isalpha(ch) == 0);
 
-	while(ch !=  EOF && is_alpha(ch) !=  0) {
+	while(ch != EOF && isalpha(ch) != 0) {
 		temp[i++] = ch;
 		ch = fgetc(fp);
 	}
-
 	temp[i] = '\0';
 }
 
 long determine_count(const char *file_name, const char *key, int ignore_case) {
-	int key_index = 0,key_len = strlen(key);
+	int key_index = 0, key_len = strlen(key);
 	long word_count = 0;
-	char ch;
-	FILE *fp = fopen(file_name,"r");
-	char temp[40];
-	int i = 0;
-	while(feof(fp) ==  0) {
-		read_word(temp,fp);
-		if(is_equal(temp,key,ignore_case))
-			word_count++;
-		//printf("%s ",temp);
+	char ch, temp[40];
+	FILE *fp = fopen(file_name, "r");
+	while(feof(fp) == 0) {
+		read_word(temp, fp);
+		if(is_equal(temp, key, ignore_case)) word_count++;
 	}
-	//printf("\nWord %s: %ld",key,word_count);
 	return word_count;
 }
 
-int main(int argc, char** argv) {
-	int i;
-
+int main(int argc, char **argv) {
 	if (argc != 2) {
-		printf("Expected argument for input file.");
+		printf("Error: Run as: ./a.out input.txt");
 		exit(-1);
 	}
-
-	int nt = 0;
+	int nt = 1;
 	printf("Enter number of threads: ");
 	scanf("%d", &nt);
 
-	for(i = 0; i < COUNT; i++) counts[i] = 0;
+	for(int i = 0; i < COUNT; i++) counts[i] = 0;
 
 	double t = omp_get_wtime();
-	#pragma omp parallel for shared(counts, search_words) private(i) num_threads(nt)
-	for(i = 0; i < COUNT; i++)
-		counts[i] = determine_count(argv[1], search_words[i], 1);
+	for(int i = 0; i < COUNT; i++)
+		determine_count(argv[1], search_words[i], 1);
 
 	t = omp_get_wtime() - t;
 
-	for(i = 0; i < COUNT; i++)
-		printf("\n%s: %ld",search_words[i],counts[i]);
+	for(int i = 0; i < COUNT; i++)
+		printf("\n%s: %ld", search_words[i], counts[i]);
 
-	printf("\nTime Taken: %lf\n",t);
+	printf("Time: %lf",  t);
 }
