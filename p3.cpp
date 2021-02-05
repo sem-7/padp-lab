35f4650fd121dae1cc513bf851ebca52d9dfc0aa
sem-7 <78492134+sem-7@users.noreply.github.com>
2021-02-05 06:16:02 +0530

    Update p3.cpp

--- p3.cpp
+++ p3.cpp
@@ -2,7 +2,6 @@
 #include<string.h>
 #include<omp.h>
 #include<iostream>
-
 using namespace std;
 
 double t = 0.0;
@@ -54,7 +53,7 @@ long CacheFriendlySieve(long n) {
          factor[n_factor++] = i;
       }
 
-   // Chops sieve into windows of size � sqrt(n)
+   // Chops sieve into windows of size √Ö sqrt(n)
    for(long window = m+1; window <= n; window += m) {
       long limit = min(window+m-1, n);
       for(long k = 0; k < n_factor; ++k)
@@ -77,59 +76,39 @@ long CacheFriendlySieve(long n) {
 long ParallelSieve(long n, int numThreads) {
    long count = 0;
    long m = (long) sqrt((double) n);
-
-   long n_factor = 0;
+   bool* composite = new bool[n+1];
+   memset(composite, 0, n);
    long* factor = new long[m];
+   long* striker = new long[m];
+   long n_factor = 0;
 
-   t = omp_get_wtime();
+   t= omp_get_wtime();
 
-   omp_set_num_threads(numThreads);
-   #pragma omp parallel
-   {
-      bool* composite = new bool[m+1];
-      long* striker = new long[m];
-
-      #pragma omp single
-      {
-         memset(composite, 0, m);
-         for(long i = 2; i <= m; ++i)
-            if(!composite[i]) {
-               ++count;
-               striker[n_factor] = Strike(composite, 2*i, i, m);
-               factor[n_factor++] = i;
-            }
-      }
-      long base = -1;
-
-      // Chops sieve into windows of size � sqrt(n)
-      #pragma omp parallel for reduction(+:count)
-      for(long window = m+1; window <= n; window += m) {
-         memset(composite, 0, m);
-         if(base != window) {
-            //Must compute stiker from scratch
-            base = window;
-            for(long k = 0; k < n_factor; ++k)
-               striker[k] = (base + factor[k]-1)/factor[k] * factor[k] - base;
-         }
-
-         long limit = min(window+m-1, n) - base;
-         for(long k = 0; k < n_factor; ++k)
-            striker[k] = Strike(composite, striker[k], factor[k], limit) - m;
-
-         for(long i = 0; i <= limit; ++i)
-            if(!composite[i])
-               ++count;
-
-         base += m;
+   for(long i = 2; i <= m; ++i)
+      if(!composite[i]) {
+         ++count;
+         striker[n_factor] = Strike(composite, 2*i, i, m);
+         factor[n_factor++] = i;
       }
 
-      delete[] striker;
-      delete[] composite;
+   // Chops sieve into windows of size √Ö sqrt(n)
+   #pragma omp parallel for reduction(+:count) num_threads(numThreads)
+   for(long window = m+1; window <= n; window += m) {
+      long limit = min(window+m-1, n);
+      for(long k = 0; k < n_factor; ++k)
+         // Strike walks window of size sqrt(n) here.
+         striker[k] = Strike(composite, striker[k], factor[k],limit);
+      for(long i = window; i <= limit; ++i)
+         if(!composite[i])
+            ++count;
    }
 
    t = omp_get_wtime() - t;
 
+   delete[] striker;
    delete[] factor;
+   delete[] composite;
+
    return count;
 }
 
