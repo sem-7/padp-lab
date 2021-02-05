23f260ad16c2163aea8902f3d16b2ab9785633c7
sem-7 <78492134+sem-7@users.noreply.github.com>
2021-02-05 08:24:02 +0530

    Bug fixes

--- p6.c
+++ p6.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <omp.h>
+#include <ctype.h>
 #include <string.h>
 #define COUNT 10
 
@@ -7,10 +8,6 @@ char search_words[20][COUNT] = {"The","around","graphics","from","by","be","any"
 	"which","various","mount"};
 long counts[COUNT];
 int line_c = 0;
-
-int is_alpha(char c) {
-	return ((c >=  65 && c <=  90) || (c >=  97 && c <=  122));
-}
 
 int is_equal(char* a,const char* key, int ignore_case) {
 	char b[20];
@@ -23,12 +20,8 @@ int is_equal(char* a,const char* key, int ignore_case) {
 	if(ignore_case != 0) {
 		int i;
 		for(i = 0; i < len_a; i++) {
-			if(a[i] > 90)
-				a[i] -=  32;
-		}
-		for(i = 0; i < len_b; i++) {
-			if(b[i] > 90)
-				b[i] -=  32;
+			a[i] = tolower(a[i]);
+			b[i] = tolower(b[i]);
 		}
 	}
 	return (strcmp(a,b) ==  0);
@@ -37,9 +30,9 @@ void read_word(char *temp, FILE *fp) {
 	int i = 0;
 	char ch;
 
-	while((ch = fgetc(fp)) !=  EOF && is_alpha(ch) ==  0);
+	while((ch = fgetc(fp)) !=  EOF && isalpha(ch) ==  0);
 
-	while(ch !=  EOF && is_alpha(ch) !=  0) {
+	while(ch !=  EOF && isalpha(ch) !=  0) {
 		temp[i++] = ch;
 		ch = fgetc(fp);
 	}
@@ -58,9 +51,7 @@ long determine_count(const char *file_name, const char *key, int ignore_case) {
 		read_word(temp,fp);
 		if(is_equal(temp,key,ignore_case))
 			word_count++;
-		//printf("%s ",temp);
 	}
-	//printf("\nWord %s: %ld",key,word_count);
 	return word_count;
 }
 
